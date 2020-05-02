e89a8a506614c567de62e60562ded515f9526f50
Pawel <52848605+pawelsz99@users.noreply.github.com>
2020-05-02 22:36:01 +0100

    About fragment and link to github

--- app/src/main/java/com/pawelsznuradev/clickchallenge/AboutFragment.kt
+++ app/src/main/java/com/pawelsznuradev/clickchallenge/AboutFragment.kt
@@ -1,5 +1,7 @@
 package com.pawelsznuradev.clickchallenge
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -24,6 +26,13 @@ class AboutFragment : Fragment() {
 
         binding.aboutButton.setOnClickListener {
             findNavController().navigate(R.id.action_aboutFragment_to_homeFragment)
+        }
+
+        binding.aboutGithub.setOnClickListener {
+          //  val gitLink = Intent(Intent.ACTION_VIEW, Uri.parse(R.string.github_link.toString()))
+            val gitLink = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/pawelsz99/ClickChallenge"))
+
+            startActivity(gitLink)
         }
 
         return binding.root
